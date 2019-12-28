5c4feba4205373cd91a8d6db6cebcb141a61cc38
Alphonse-xu.github.io <bluerailgun@outlook.com>
2019-12-28 21:39:13 +0000

    helloworld

---
+++ Cousework1_helloworld/Cousework1_helloworld.cpp
@@ -0,0 +1,48 @@
+﻿// Cousework1_helloworld.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	int a = -1;
+	char b = 'n';
+	bool c =  true;
+	double d = 1.1;
+	int userNUM1;
+	int userNUM2;
+	int NumSum;
+	int NumProduct;
+	std::string Myname;
+
+	std::cout << "enter your name" << std::endl;
+	std::cin >> Myname;
+
+	std::cout << "enter your num" << std::endl;
+	std::cin >> userNUM1;
+	std::cin >> userNUM2;
+
+	NumSum = userNUM1 + userNUM2;
+	NumProduct = userNUM1 * userNUM2;
+
+	std::cout << "your name is "<< Myname << std::endl;
+	std::cout << "sum is " << NumSum << std::endl;
+	std::cout << "product is " << NumProduct << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << c << std::endl;
+	std::cout << d << std::endl;
+    std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
